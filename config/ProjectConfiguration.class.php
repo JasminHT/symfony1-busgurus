1c751873dd88a5a3ca14df8b8cb5b6172e7b3e3b
JasminHT <JasminHT@users.noreply.github.com>
2024-05-05 15:25:55 -0400

    Add missing configuration files

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,16 +1,37 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../vendor/lexpress/symfony1/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+
+  static protected $zendLoaded = false;
+
+  //dont worry this is never called
+  static public function registerZend()
+  {
+      if (self::$zendLoaded)
+      {
+          return;
+      }
+      set_include_path(sfConfig::get('sf_lib_dir').'/vendor'.PATH_SEPARATOR.get_include_path());
+      require_once sfConfig::get('sf_lib_dir').'/vendor/Zend/Loader/Autoloader.php';
+      Zend_Loader_Autoloader::getInstance();
+      self::$zendLoaded = true;
+  }
+
+  //called once on page load
   public function setup()
   {
 
-  	$this->setWebDir($this->getRootDir().'../public_html/busgurus');
-    $this->enableAllPluginsExcept(array('sfDoctrinePlugin'));
-    $this->enablePlugins('sfPropelPlugin');
+    //leads into the public files
+  	$this->setWebDir($this->getRootDir().'/../www/buscouncil/busgurus/index.php/');
+    $this->enablePlugins('sfPropelORMPlugin');
     $this->enablePlugins('sfGuardPlugin');
+    $this->enablePlugins('sfSimpleCMSPlugin');
+    $this->enablePlugins('sfPropelActAsNestedSetBehaviorPlugin');
+    $this->enablePlugins('sfFormExtraPlugin');
+
   }
 }
